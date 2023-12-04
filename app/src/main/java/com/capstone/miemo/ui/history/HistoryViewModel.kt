1521d65c14a389f8f005e603dee3db9f83af2e5b
azkazaki27 <azkazaki27@gmail.com>
2023-12-04 11:33:54 +0700

    bottom navbar

--- app/src/main/java/com/capstone/miemo/ui/history/HistoryViewModel.kt
+++ app/src/main/java/com/capstone/miemo/ui/history/HistoryViewModel.kt
@@ -1,10 +1,10 @@
-package com.capstone.miemo.ui.dashboard
+package com.capstone.miemo.ui.history
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class DashboardViewModel : ViewModel() {
+class HistoryViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is dashboard Fragment"

de807d5344847f76e400ca78fc15a6827681ac71
azkazaki27 <azkazaki27@gmail.com>
2023-12-18 11:24:25 +0700

    added logout

--- app/src/main/java/com/capstone/miemo/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/capstone/miemo/ui/profile/ProfileActivity.kt
@@ -1,4 +1,6 @@
 package com.capstone.miemo.ui.profile
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
@@ -6,13 +8,21 @@ import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.miemo.R
+import com.capstone.miemo.databinding.ActivityProfileBinding
+import com.capstone.miemo.ui.auth.RegisterActivity
+import com.google.firebase.Firebase
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.auth
 
 class ProfileActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityProfileBinding
+    private lateinit var auth: FirebaseAuth
 
     // Declare your UI elements here
     private lateinit var tvProfile: TextView
     private lateinit var imgProfile: ImageView
-    private lateinit var tvEmail: TextView
+    private lateinit var tvUsername: TextView
     private lateinit var usernameEditText: EditText
     private lateinit var btnLogout: Button
 
@@ -22,17 +32,21 @@ class ProfileActivity : AppCompatActivity() {
 
         tvProfile = findViewById(R.id.tv_profile)
         imgProfile = findViewById(R.id.img_profile)
-        tvEmail = findViewById(R.id.tv_email)
+        tvUsername = findViewById(R.id.tv_username)
         usernameEditText = findViewById(R.id.usernameEditText)
         btnLogout = findViewById(R.id.btn_logout)
 
-        // Now you can use these variables to manipulate the UI as needed
-        // For example, you can set text or click listeners
+        val sharedPreferences = getPreferences(MODE_PRIVATE)
+        val savedUsername = sharedPreferences.getString("username", "")
+        binding.usernameEditText.hint = "${savedUsername}"
 
-        // Example: Set a click listener for the logout button
         btnLogout.setOnClickListener {
-            // Handle logout button click
-            // Add your logic here
+            Firebase.auth.signOut()
+            register()
         }
+    }
+    private fun register() {
+        val intent = Intent(this, RegisterActivity::class.java)
+        startActivity(intent)
     }
 }

d92ff5a8bc25893f67897d784d8c496a4ba3b4da
azkazaki27 <azkazaki27@gmail.com>
2023-12-18 23:01:04 +0700

    added username edit

--- app/src/main/java/com/capstone/miemo/ui/auth/RegisterActivity.kt
+++ app/src/main/java/com/capstone/miemo/ui/auth/RegisterActivity.kt
@@ -2,7 +2,6 @@ package com.capstone.miemo.ui.auth
 
 import android.content.ContentValues.TAG
 import android.content.Context
-import android.content.SharedPreferences
 import android.content.Intent
 import android.os.Build
 import android.os.Bundle
@@ -10,14 +9,13 @@ import android.util.Log
 import android.view.View
 import android.view.WindowInsets
 import android.view.WindowManager
+import android.widget.EditText
 import android.widget.Toast
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.miemo.R
 import com.capstone.miemo.databinding.ActivitySigninBinding
 import com.capstone.miemo.ui.ViewModelFactory
-import com.capstone.miemo.ui.auth.AuthViewModel
-import com.capstone.miemo.data.Result
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 
@@ -27,16 +25,15 @@ class RegisterActivity : AppCompatActivity() { private lateinit var binding: Act
     }
 
     private lateinit var auth: FirebaseAuth
-    private lateinit var sharedPreferences: SharedPreferences
-
+    private lateinit var usernameEditText: EditText
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         binding = ActivitySigninBinding.inflate(layoutInflater)
         setContentView(binding.root)
+        usernameEditText = findViewById(R.id.ed_username)
 
         auth = FirebaseAuth.getInstance()
-        sharedPreferences = getPreferences(Context.MODE_PRIVATE)
 
         setupView()
     }
@@ -44,7 +41,15 @@ class RegisterActivity : AppCompatActivity() { private lateinit var binding: Act
     private fun setupView() {
         hideSystemUI()
 
-        binding.btnRegister.setOnClickListener { register() }
+        binding.btnRegister.setOnClickListener {
+            val sharedPreferences = getPreferences(Context.MODE_PRIVATE)
+            val enteredUsername: String = usernameEditText.text.toString()
+            with(sharedPreferences.edit()) {
+                putString("username", enteredUsername)
+                apply()
+            }
+            register()
+        }
         binding.tvRegisterToLogin.setOnClickListener { gotoLogin() }
     }
 
@@ -63,13 +68,8 @@ class RegisterActivity : AppCompatActivity() { private lateinit var binding: Act
 
     private fun register() {
         binding.progressBar.visibility = View.VISIBLE
-        val name = binding.edUsername.text.toString()
         val email = binding.edEmail.text.toString()
         val password = binding.edPassword.text.toString()
-
-        // Save the username to SharedPreferences
-        saveUsername(name)
-
 
         // Use createUserWithEmailAndPassword to register a new user
         auth.createUserWithEmailAndPassword(email, password)
@@ -93,7 +93,6 @@ class RegisterActivity : AppCompatActivity() { private lateinit var binding: Act
             }
     }
 
-
     private fun updateUI(user: FirebaseUser?) {
         if (user != null) {
             Toast.makeText(this, "Registration successful", Toast.LENGTH_SHORT).show()
@@ -101,12 +100,6 @@ class RegisterActivity : AppCompatActivity() { private lateinit var binding: Act
             // For example, you might want to navigate to the login screen
             gotoLogin()
         }
-    }
-
-    private fun saveUsername(username: String) {
-        val editor = sharedPreferences.edit()
-        editor.putString("username", username)
-        editor.apply()
     }
 
     private fun gotoLogin() {
--- app/src/main/java/com/capstone/miemo/ui/profile/ProfileActivity.kt
+++ app/src/main/java/com/capstone/miemo/ui/profile/ProfileActivity.kt
@@ -1,11 +1,12 @@
 package com.capstone.miemo.ui.profile
-import android.content.Context
+
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
 import android.widget.EditText
 import android.widget.ImageView
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import com.capstone.miemo.R
 import com.capstone.miemo.databinding.ActivityProfileBinding
@@ -19,26 +20,42 @@ class ProfileActivity : AppCompatActivity() {
     private lateinit var binding: ActivityProfileBinding
     private lateinit var auth: FirebaseAuth
 
-    // Declare your UI elements here
     private lateinit var tvProfile: TextView
     private lateinit var imgProfile: ImageView
     private lateinit var tvUsername: TextView
     private lateinit var usernameEditText: EditText
     private lateinit var btnLogout: Button
+    private lateinit var btnSave: Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_profile)
+
+        binding = ActivityProfileBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         tvProfile = findViewById(R.id.tv_profile)
         imgProfile = findViewById(R.id.img_profile)
         tvUsername = findViewById(R.id.tv_username)
         usernameEditText = findViewById(R.id.usernameEditText)
         btnLogout = findViewById(R.id.btn_logout)
+        btnSave = findViewById(R.id.btn_save)
 
         val sharedPreferences = getPreferences(MODE_PRIVATE)
-        val savedUsername = sharedPreferences.getString("username", "")
-        binding.usernameEditText.hint = "${savedUsername}"
+
+        val savedUsername = sharedPreferences.getString("username", null)
+        usernameEditText.setText(savedUsername)
+
+        btnSave.setOnClickListener {
+            // Get the entered username
+            val enteredUsername: String = usernameEditText.text.toString()
+
+            // Save the username in SharedPreferences
+            with(sharedPreferences.edit()) {
+                putString("username", enteredUsername)
+                apply()
+            }
+            Toast.makeText(this, "Username saved successfully", Toast.LENGTH_SHORT).show()
+        }
 
         btnLogout.setOnClickListener {
             Firebase.auth.signOut()

51d68662aaafe15bdd474b45313ccb3bab7d2b7f
Hernawan Abidin <hernaone1245@gmail.com>
2023-12-18 10:57:45 +0700

    fix MainActivity cannot start

--- app/src/main/java/com/capstone/miemo/ui/home/HomeFragment.kt
+++ app/src/main/java/com/capstone/miemo/ui/home/HomeFragment.kt
@@ -5,9 +5,13 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.activity.viewModels
 import androidx.fragment.app.Fragment
+import androidx.fragment.app.viewModels
 import androidx.lifecycle.ViewModelProvider
 import com.capstone.miemo.databinding.FragmentHomeBinding
+import com.capstone.miemo.ui.ViewModelFactory
+import com.capstone.miemo.ui.auth.AuthViewModel
 
 class  HomeFragment : Fragment() {
 
@@ -17,27 +21,37 @@ class  HomeFragment : Fragment() {
     // onDestroyView.
     private val binding get() = _binding!!
 
+    //private lateinit var homeViewModel: HomeViewModel
+
+    private val homeViewModel: HomeViewModel by viewModels {
+        ViewModelFactory(requireActivity())
+    }
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
-        val homeViewModel =
-            ViewModelProvider(this).get(HomeViewModel::class.java)
 
         _binding = FragmentHomeBinding.inflate(inflater, container, false)
         val root: View = binding.root
 
-        val textView: TextView = binding.textHome
-        homeViewModel.text.observe(viewLifecycleOwner) {
-            textView.text = it
-        }
+        return root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+       // homeViewModel = ViewModelProvider(requireActivity())[HomeViewModel::class.java]
 
         binding.btnAdd.setOnClickListener {
             showDialog()
         }
 
-        return root
+        val textView: TextView = binding.textHome
+        homeViewModel.text.observe(viewLifecycleOwner) {
+            textView.text = it
+        }
     }
 
     private fun showDialog() {

536c7b59eed1b41e2dc76a6ec79b9b347f3e075c
Hernawan Abidin <hernaone1245@gmail.com>
2023-12-22 00:47:51 +0700

    add json file contains quote

---
+++ app/src/main/java/com/capstone/miemo/data/local/output/Output.kt
@@ -0,0 +1,19 @@
+package com.capstone.miemo.data.local.output
+
+data class Output (
+    val emotions: List<String>,
+    val messages: Messages
+)
+
+data class Messages (
+    val sadness: List<Quotes>,
+    val anger: List<Quotes>,
+    val fear: List<Quotes>,
+    val love: List<Quotes>,
+    val joy: List<Quotes>,
+    val surprise: List<Quotes>
+)
+
+data class Quotes (
+    val message: String
+)
